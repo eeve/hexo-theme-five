07e1a3eafb391ca0af204011011b1145c58d180f
eeve <eeveme@gmail.com>
2016-11-11 18:05:59 +0800

    build

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,9 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var env = env || (process.env.NODE_ENV || 'development');
-var debug = env === 'development';
-console.log('======= '+ debug ? 'development' : 'production' +' =======');
-
 function path_for(_path){
 	//兼容 Windows 环境
 	return path.resolve(__dirname,_path);
-}
-
-var plugins = [
-	new webpack.DefinePlugin({
-		__DEV__: debug
-	}),
-	// new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js'),
-	// new webpack.ProvidePlugin({
-	// 	$: "jquery",
-	// 	jQuery: "jquery",
-	// 	"window.jQuery": "jquery"
-	// }),
-	new webpack.BannerPlugin('This file is created by eeve.'),
-	new webpack.NoErrorsPlugin()
-];
-
-if(!debug){
-	plugins.push(
-		new webpack.optimize.UglifyJsPlugin({
-			test: /(\.jsx|\.js)$/,
-			compress: {
-				warnings: false
-			},
-			// mangle: false
-		})
-	);
 }
 
 var config = {
@@ -44,7 +14,6 @@ var config = {
 		path: path_for('./source/js'),
 		filename:'[name].js'
 	},
-	plugins: plugins,
 	module:{
 		loaders:[
 			{ test: /\.css$/, loader: 'style!css' }
